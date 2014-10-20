c5b0e7b8e362c192f021dc0c4f835ae4f6a3a67e
Fernando L. Canizo (aka conan) <conan@muriandre.com>
2014-10-20 07:50:05 -0300

    Ad basic structures: Player, Bullets and Invaders

--- game.js
+++ game.js
@@ -4,11 +4,16 @@ var si = {}; // global object for common settings
 	var Game = function (canvasId) {
 		si.canvas = document.getElementById(canvasId);
 		si.pg = si.canvas.getContext("2d");
+		si.players = [new Player()];
+		si.bullets = [];
+		si.invaders = [];
 
+		// helper array to process all draw functions at once
+		this.bodies = si.players.concat(si.bullets, si.invaders);
 		var self = this;
 		var tick = function () {
 			self.update();
-			self.draw(screen);
+			self.draw();
 			requestAnimationFrame(tick);
 		};
 
@@ -17,12 +22,69 @@ var si = {}; // global object for common settings
 
 	Game.prototype = {
 		update: function () {
-			console.log('hi');
+		},
+
+		draw: function () {
+			si.pg.fillRect(0, 0, 30, 30);
+		}
+	};
+
+	var Player = function () {
+		this.color = 'red';
+		this.size = {
+			x: 15,
+			y: 15
+		};
+		this.center = {
+			x: si.pg.width / 2,
+			y: si.pg.height - this.size.y
+		};
+	};
+
+	Player.prototype = {
+		update: function () {
 		},
 
 		draw: function () {
 		}
 	};
+
+
+	var Bullet = function (center, color) {
+		this.color = color || 'black'; // default to black
+		this.size = {
+			x: 3,
+			y: 3
+		};
+		this.center = center;
+	};
+
+	Bullet.prototype = {
+		update: function () {
+		},
+
+		draw: function () {
+		}
+	};
+
+
+	var Invader = function (center) {
+		this.color = color;
+		this.size = {
+			x: 10,
+			y: 10
+		};
+		this.center = center;
+	};
+
+	Invader.prototype = {
+		update: function () {
+		},
+
+		draw: function () {
+		}
+	};
+
 
 	window.onload = function () {
 		new Game("playground");
