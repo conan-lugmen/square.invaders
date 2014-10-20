13f60beb2f551d8015b52898a7a8812f5586a4ad
Fernando L. Canizo (aka conan) <conan@muriandre.com>
2014-10-20 07:30:41 -0300

    Basic Game structure working

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log('bootstrapped');
+var si = {}; // global object for common settings
+
+;(function() {
+	var Game = function (canvasId) {
+		si.canvas = document.getElementById(canvasId);
+		si.pg = si.canvas.getContext("2d");
+		console.log('Game working');
+	};
+
+	Game.prototype = {
+	};
+
+	window.onload = function () {
+		new Game("playground");
+	};
+}());
